2a6b1e86138a75d63a63647e868f7b8528b5bc22
randered <68559794+randered@users.noreply.github.com>
2021-11-01 15:29:12 +0200

    Adding Building exercise

---
+++ TrainingOne/src/BuildingWithFloors/Building.java
@@ -0,0 +1,24 @@
+package BuildingWithFloors;
+
+import java.util.Scanner;
+
+public class Building {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please Enter the number of Floors: ");
+        int numberOfFloors = scanner.nextInt();
+        System.out.print("Please Enter the number of Estates per Floor: ");
+        int estatesPerFloor = scanner.nextInt();
+
+        for (int floor = numberOfFloors; floor >= 1; floor--) {
+            for (int estates = 1; estates <= estatesPerFloor; estates++) {
+                String typeOfEstate = "A";
+                if (floor % 2 == 0) typeOfEstate = "O";
+                if (floor == numberOfFloors) typeOfEstate = "L";
+                System.out.print(typeOfEstate + floor + estates + " ");
+            }
+            System.out.println();
+        }
+    }
+}
