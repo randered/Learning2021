4c2bac758fa4b998fd66dd244e5fe73ce19126c2
randered <68559794+randered@users.noreply.github.com>
2021-11-30 18:38:55 +0200

    Saved the TreeImpl to json.

--- TreeExerc/src/BinaryTreeImpl/Main.java
+++ TreeExerc/src/BinaryTreeImpl/Main.java
@@ -1,16 +1,26 @@
 package BinaryTreeImpl;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 public class Main {
     static BSTNode mainRoot;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Gson gson = new GsonBuilder().create();
+        FileWriter fileWriter = new FileWriter("Student.json");
         BinaryTreeImpl.insert(mainRoot,15);
         BinaryTreeImpl.insert(mainRoot,10);
         BinaryTreeImpl.insert(mainRoot,20);
         BinaryTreeImpl.insert(mainRoot,25);
         BinaryTreeImpl.insert(mainRoot,8);
         BinaryTreeImpl.insert(mainRoot,12);
+        gson.toJson(mainRoot, fileWriter);
         BinaryTreeImpl.checkIfExist(mainRoot,12);
-        
+        fileWriter.close();
+
     }
 }
